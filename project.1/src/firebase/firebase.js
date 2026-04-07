bc7e8ff388d693e95e8e5aa5e08d5dfe99b3c288
Cholpon <januzakova.cholpon@gmail.com>
2026-04-07 12:29:37 +0600

    add firebase

--- project.1/src/firebase/firebase.js
+++ project.1/src/firebase/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, serverTimestamp } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyBy3hwg3wDeDCEYO5MZ1_AIjXUV5wZ1u9E",
   authDomain: "project1-374c1.firebaseapp.com",
@@ -16,6 +12,7 @@ const firebaseConfig = {
   measurementId: "G-Z3H1S7QZZX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
+export { serverTimestamp};
--- project.1/src/pages/Home.jsx
+++ project.1/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import {
   deleteDoc,
   doc,
 } from "firebase/firestore";
+
 import { db } from "../firebase/firebase";
 
 import StudentForm from "../components/StudentForm";
@@ -20,8 +21,12 @@ function Home() {
   const [status, setStatus] = useState("all");
   const [editStudent, setEditStudent] = useState(null);
 
+  // 🔥 LOAD
   const fetchStudents = async () => {
-    let q = query(collection(db, "students"), orderBy("createdAt", "desc"));
+    const q = query(
+      collection(db, "students"),
+      orderBy("createdAt", "desc")
+    );
 
     const data = await getDocs(q);
     const arr = data.docs.map((doc) => ({
@@ -36,15 +41,16 @@ function Home() {
     fetchStudents();
   }, []);
 
+  // ❌ DELETE
   const handleDelete = async (id) => {
     await deleteDoc(doc(db, "students", id));
     fetchStudents();
   };
 
-  // 🔍 frontend search
+  // 🔍 FILTER
   const filteredStudents = students
     .filter((s) =>
-      s.name.toLowerCase().includes(search.toLowerCase())
+      s.name?.toLowerCase().includes(search.toLowerCase())
     )
     .filter((s) => (status === "all" ? true : s.status === status));
 
